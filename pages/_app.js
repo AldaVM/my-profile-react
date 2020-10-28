d596a8c7f4c961ad4d0906dc096c7fd8ba89ea1b
AldaVM <aldairvm95@gmail.com>
2020-10-27 22:46:27 -0500

    transitions effects are added when switching pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,38 @@
 import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { CacheProvider } from "@emotion/core";
 import { cache } from "emotion";
 import { globalStyle } from "../styles";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <>
-      <CacheProvider value={cache}>
-        {globalStyle}
-        <Component {...pageProps} />
-      </CacheProvider>
+      <AnimatePresence>
+        <motion.div
+          key={router.route}
+          initial="pageInitial"
+          animate="pageAnimate"
+          exit="pageExit"
+          variants={{
+            pageInitial: {
+              opacity: 0,
+            },
+            pageAnimate: {
+              opacity: 1,
+            },
+            pageExit: {
+              backgroundColor: "white",
+              filter: `invert()`,
+              opacity: 0,
+            },
+          }}
+        >
+          <CacheProvider value={cache}>
+            {globalStyle}
+            <Component {...pageProps} />
+          </CacheProvider>
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
--- src/components/view/aboutme/CardDescription/index.js
+++ src/components/view/aboutme/CardDescription/index.js
@@ -1,34 +1,54 @@
+import { motion } from "framer-motion";
 import { Section, FormatImage } from "../../../common";
 import { AboutMeContainer } from "./Styled";
 
 export default function CardDescription() {
   return (
     <Section bg="#212428" id="aboutme">
-      <AboutMeContainer>
-        <FormatImage
-          size="200px"
-          picture="https://www.medioambiente.net/wp-content/uploads/guepardo.jpg"
-          description="Foto de Aldair Valencia - Desarrollador Web"
-          square={true}
-        />
-        <h1>Hola, soy Aldair</h1>
-        <p>
-          Trabajo como <strong>JavaScript Developer</strong>, creando
-          aplicaciones y sitios web modernos, utilizando principalmente
-          tecnologías como
-          <strong> HTML</strong>, <strong>CSS</strong> y
-          <strong> JavaScript</strong>, además, de participar en proyectos
-          empresariales dedicados al desarrollo de soluciones de{" "}
-          <strong>Ecommerce</strong>.
-        </p>
-        <p>
-          Paralelamente sigo formandome profesionalmente como desarrollador de
-          software, además, de estar constantemente aprendiendo de forma
-          autodidacta. En mis tiempos libres me gusta aprender nuevas
-          tecnologías, mantenerme al día con las extrañas tendencias de los
-          avances del software y compartir mis conocimientos.
-        </p>
-      </AboutMeContainer>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+              delay: 0.4,
+            },
+          },
+        }}
+      >
+        <AboutMeContainer>
+          <FormatImage
+            size="200px"
+            picture="https://www.medioambiente.net/wp-content/uploads/guepardo.jpg"
+            description="Foto de Aldair Valencia - Desarrollador Web"
+            square={true}
+          />
+
+          <h1>Hola, soy Aldair</h1>
+          <p>
+            Trabajo como <strong>JavaScript Developer</strong>, creando
+            aplicaciones y sitios web modernos, utilizando principalmente
+            tecnologías como
+            <strong> HTML</strong>, <strong>CSS</strong> y
+            <strong> JavaScript</strong>, además, de participar en proyectos
+            empresariales dedicados al desarrollo de soluciones de{" "}
+            <strong>Ecommerce</strong>.
+          </p>
+          <p>
+            Paralelamente sigo formandome profesionalmente como desarrollador de
+            software, además, de estar constantemente aprendiendo de forma
+            autodidacta. En mis tiempos libres me gusta aprender nuevas
+            tecnologías, mantenerme al día con las extrañas tendencias de los
+            avances del software y compartir mis conocimientos.
+          </p>
+        </AboutMeContainer>
+      </motion.div>
     </Section>
   );
 }
